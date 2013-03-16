df548d8578b270edd80147fc303a68fd65edfdc4
renga <mr.rengaswamy@gmail.com>
2013-03-16 13:01:45 +0800

    Changed themes and added few more helpers

--- Birdfun/gen/com/fyp/birdfun/R.java
+++ Birdfun/gen/com/fyp/birdfun/R.java
@@ -21,40 +21,44 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int btn_default_disabled_focused_holo_dark=0x7f020001;
+        public static final int btn_default_disabled_holo_dark=0x7f020002;
+        public static final int btn_default_focused_holo_dark=0x7f020003;
+        public static final int btn_default_holo_dark=0x7f020004;
+        public static final int btn_default_normal_holo_dark=0x7f020005;
+        public static final int btn_default_pressed_holo_dark=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f08000c;
         public static final int btnfantasticfeathers=0x7f080001;
+        public static final int btnleaderboard=0x7f080008;
         public static final int btnlogin=0x7f080003;
+        public static final int btnlogins=0x7f080006;
+        public static final int btnplays=0x7f080005;
+        public static final int btnquit=0x7f080009;
+        public static final int btnregiste=0x7f080007;
         public static final int btnregister=0x7f080004;
         public static final int btnsavetheeggs=0x7f080000;
+        public static final int btnsavethepeggs=0x7f08000a;
         public static final int btntheweapon=0x7f080002;
+        public static final int btntheweaponr=0x7f08000b;
     }
     public static final class layout {
         public static final int login_user=0x7f030000;
         public static final int main_screen=0x7f030001;
-        public static final int register_user=0x7f030002;
+        public static final int play_screen=0x7f030002;
+        public static final int register_user=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -66,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f060002;
+        public static final int BirdFunTheme=0x7f060000;
+        /**  Button Therme 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int ButtonAppTheme=0x7f060001;
     }
 }
---
+++ Birdfun/src/com/fyp/birdfun/SpalshScreenActivity.java
@@ -0,0 +1,7 @@
+package com.fyp.birdfun;
+
+import android.app.Activity;
+
+public class SpalshScreenActivity extends Activity {
+
+}
