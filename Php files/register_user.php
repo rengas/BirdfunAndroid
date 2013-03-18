63e6276a0825c0912c39fc5c9bd5778a4632bb46
renga <mr.rengaswamy@gmail.com>
2013-03-18 22:50:30 +0800

    User PHP updated

--- Php files/register_user.php
+++ Php files/register_user.php
@@ -1,16 +1,16 @@
 <?php
  
 /*
- * Following code will create a new product row
- * All product details are read from HTTP Post Request
+ * Following code will create a new user  row
+ * All user details are read from HTTP Post Request
  */
  
 // array for JSON response
 $response = array();
  
 // check for required fields
-if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['school']) && isset($_POST['login']) && isset($_POST['password'])) {
- 
+if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['school']) && isset($_POST['login']) && isset($_POST['password'])) 
+{
     $name = $_POST['name'];
     $age = $_POST['age'];
     $school = $_POST['school'];
@@ -21,27 +21,37 @@ if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['school']) && 
  
     // connecting to db
     $db = new DB_CONNECT();
- 
-    // mysql inserting a new row
-    $result = mysql_query("INSERT INTO users(name, age, school,login,password) VALUES('$name', '$age', '$school','$login','$password')");
- 
+    
+		$search =mysql_query("SELECT *FROM users Where login='$login'");
+   // Mysql_num_row is counting table row
+		$count=mysql_num_rows($search);
     // check if row inserted or not
-    if ($result) {
+    if ($count==1) {
         // successfully inserted into database
-        $response["success"] = 1;
-        $response["message"] = "Product successfully created.";
- 
-        // echoing JSON response
-        echo json_encode($response);
-    } else {
-        // failed to insert row
         $response["success"] = 0;
-        $response["message"] = "Oops! An error occurred.";
- 
+        $response["message"] = "User already Exits";
         // echoing JSON response
         echo json_encode($response);
     }
-}
+	else {
+    	// mysql inserting a new row
+		$result = mysql_query("INSERT INTO users(name, age, school,login,password) VALUES('$name', '$age', '$school','$login','$password')");
+
+			if($result){
+			//sucessfully inserted 
+			$response["success"] = 1;
+			$response["message"] = "User Account created succesfully.";
+			}
+			else{
+			// failed to insert row
+			$response["success"] = 0;
+			$response["message"] = "failed to create account";
+			}
+
+        // echoing JSON response
+        echo json_encode($response);
+    }
+	}
  else {
     // required field is missing
     $response["success"] = 0;
